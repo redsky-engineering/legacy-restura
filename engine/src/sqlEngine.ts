59971df99e9237123ef4ab2f2db2e3e7d200370d
Joshua Hintze <joshua.hintze@gmail.com>
2023-05-31 11:38:13 -0600

    - fixes for subqueries with alias joins

--- engine/src/sqlEngine.ts
+++ engine/src/sqlEngine.ts
@@ -142,7 +142,7 @@ class SqlEngine {
 						req.requesterDetails.role,
 						schema,
 						nestedItem,
-						routeData.joins
+						[...routeData.joins, ...item.subquery!.joins]
 					);
 				})
 			)
@@ -160,7 +160,7 @@ class SqlEngine {
 												req.requesterDetails.role,
 												schema,
 												nestedItem,
-												routeData.joins
+												[...routeData.joins, ...item.subquery!.joins]
 											)
 										) {
 											return;
@@ -234,7 +234,8 @@ class SqlEngine {
 
 		let selectColumns: Restura.ResponseData[] = [];
 		routeData.response.forEach((item) => {
-			if (this.doesRoleHavePermissionToColumn(userRole, schema, item, routeData.joins) || item.subquery)
+			// For a subquery, we will check the permission when generating the subquery statement, so pass it through
+			if (item.subquery || this.doesRoleHavePermissionToColumn(userRole, schema, item, routeData.joins))
 				selectColumns.push(item);
 		});
 		if (!selectColumns.length) throw new RsError('UNAUTHORIZED', `You do not have permission to access this data.`);
