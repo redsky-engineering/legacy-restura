f5585722d2eb3d5fc4ee973ee1572037180ed3d7
Joshua Hintze <joshua.hintze@gmail.com>
2024-02-04 16:44:20 -0700

    - Update default schema

--- engine/src/defaultSchema.ts
+++ engine/src/defaultSchema.ts
@@ -32,7 +32,15 @@ const defaultSchema: Restura.Schema = {
 				{ roles: [], name: 'role', type: 'ENUM', isNullable: false, value: "'admin','technician'" },
 				{ roles: [], name: 'permissionLogin', type: 'BOOLEAN', isNullable: false, default: '1' },
 				{ roles: [], name: 'lastLoginOn', type: 'DATETIME', isNullable: true },
-				{ roles: [], name: 'phone', type: 'VARCHAR', length: 30, isNullable: true }
+				{ roles: [], name: 'phone', type: 'VARCHAR', length: 30, isNullable: true },
+				{
+					roles: [],
+					name: 'loginDisabledOn',
+					type: 'DATETIME',
+					isNullable: true,
+					comment: 'When user was disabled'
+				},
+				{ roles: [], name: 'passwordResetGuid', type: 'VARCHAR', length: 100, isNullable: true }
 			],
 			foreignKeys: [
 				{
@@ -52,6 +60,20 @@ const defaultSchema: Restura.Schema = {
 					isPrimaryKey: false,
 					order: 'ASC',
 					name: 'user_companyId_index'
+				},
+				{
+					name: 'user_email_unique_index',
+					columns: ['email'],
+					order: 'ASC',
+					isPrimaryKey: false,
+					isUnique: true
+				},
+				{
+					name: 'user_passwordResetGuid_index',
+					isUnique: false,
+					order: 'ASC',
+					columns: ['passwordResetGuid'],
+					isPrimaryKey: false
 				}
 			],
 			roles: []
@@ -122,6 +144,129 @@ const defaultSchema: Restura.Schema = {
 					description: 'Refresh an old, possibly expired token and returns a new token.',
 					path: '/user/re-auth',
 					roles: []
+				},
+				{
+					type: 'CUSTOM_ONE',
+					responseType: 'FilteredUser',
+					request: [
+						{
+							name: 'firstName',
+							required: true,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'lastName',
+							required: true,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'email',
+							required: true,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'role',
+							required: true,
+							validator: [{ type: 'ONE_OF', value: ['admin', 'user'] }]
+						},
+						{
+							name: 'password',
+							required: true,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'phone',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						}
+					],
+					method: 'POST',
+					name: 'Create User',
+					description: 'Creates a user',
+					path: '/user',
+					roles: ['admin']
+				},
+				{
+					type: 'CUSTOM_ONE',
+					responseType: 'FilteredUser',
+					request: [
+						{ name: 'id', required: true, validator: [{ type: 'TYPE_CHECK', value: 'number' }] },
+						{
+							name: 'firstName',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'lastName',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'email',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'role',
+							required: false,
+							validator: [{ type: 'ONE_OF', value: ['admin', 'technician'] }]
+						},
+						{
+							name: 'password',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						}
+					],
+					method: 'PATCH',
+					name: 'Update User',
+					description: 'Update an existing user.',
+					path: '/user',
+					roles: ['admin']
+				},
+				{
+					type: 'ONE',
+					method: 'PATCH',
+					name: 'Update my user',
+					description: 'Update my user',
+					path: '/user/me',
+					table: 'user',
+					roles: ['user', 'admin'],
+					request: [
+						{
+							name: 'firstName',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'lastName',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'email',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'phone',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						},
+						{
+							name: 'password',
+							required: false,
+							validator: [{ type: 'TYPE_CHECK', value: 'string' }]
+						}
+					],
+					joins: [],
+					response: [
+						{ name: 'id', selector: 'user.id' },
+						{ name: 'firstName', selector: 'user.firstName' },
+						{ name: 'lastName', selector: 'user.lastName' },
+						{ name: 'email', selector: 'user.email' }
+					],
+					assignments: [],
+					where: [{ tableName: 'user', columnName: 'id', operator: '=', value: '#userId' }]
 				}
 			]
 		}
@@ -131,5 +276,4 @@ const defaultSchema: Restura.Schema = {
 	customTypes:
 		'export interface FilteredUser {\n    id: number;\n\tcompanyId: number;\n\tfirstName: string;\n\tlastName: string;\n\temail: string;\n\trole: string;\n\tphone: string;\n\tlastLoginOn: string;\n}\n\nexport interface LoginResponse {\n\ttoken: string;\n\texpiresOn: string;\n\trefreshToken: string;\n\tuser: FilteredUser;\n}\n\nexport interface ReAuthResponse {\n\ttoken: string;\n\trefreshToken?: string;\n\texpiresOn: string;\n}\n'
 };
-
 export default defaultSchema;
